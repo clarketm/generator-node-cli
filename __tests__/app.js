ca2465b335f39a553382aa117e840d5671f05771
Travis Clarke <travis.m.clarke@gmail.com>
2018-05-26 21:45:58 -0700

    fix tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -29,7 +29,7 @@ describe("generator-node-cli:app", () => {
       "README.md",
       ".gitignore",
       "package.json",
-      `bin/${binName}.j`,
+      `bin/${binName}.js`,
       `lib/${binName}.js`,
       `test/${binName}.test.js`,
       "jest.config.js"
